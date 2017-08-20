92542bd6f3aa850a5a16eb1305778d7c51704c23
Nghe Pham <ilovetayninh@gmail.com>
2017-08-20 10:43:43 +0700

    [Update]Phone dial push(1)

--- app/src/main/java/pvnghe/patternslibrary/activity/MainActivity.java
+++ app/src/main/java/pvnghe/patternslibrary/activity/MainActivity.java
@@ -1,22 +1,13 @@
 package pvnghe.patternslibrary.activity;
 
-import android.Manifest;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import pvnghe.patternslibrary.R;
 import pvnghe.patternslibrary.designpatterns.Task;
